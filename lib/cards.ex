702079a3a78a43e9e0af2511eee0cf3909b987df
bilal <bilal@localhost.localdomain>
2017-11-26 16:14:08 +0500

    deal, contains functions added

--- lib/cards.ex
+++ lib/cards.ex
@@ -6,4 +6,16 @@ defmodule Cards do
       "#{value} of #{suit}" 
     end
   end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)    
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck,card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
 end
